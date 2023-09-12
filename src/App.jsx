6823273c4a8057f67b8facdf9b9c87e7989db7ab
Litalhag <litalhag@gmail.com>
2023-09-12 16:43:45 +0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
-import UserForm from './pages/UserForm';
+import UserForm from './pages/UserForm'
 
 function App() {
-
   return (
     <>
+      <header className="flex">
+        <h1>Submit Registration</h1>
+        <div className="line"></div>
+      </header>
       <UserForm />
     </>
   )
